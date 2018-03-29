75c44468d1ffb0223fa8913e0e372148fcff61c7
Tim <35234836+eggscendance@users.noreply.github.com>
2018-03-29 14:54:27 -0300

    Update index.php
    Basic interface for INSERT.

---
+++ index.php
@@ -0,0 +1,6 @@
+<!--BASIC user interface-->
+<p>Insert Records:</p>
+<form action="processFiles.php" method="post" enctype="multipart/form-data">
+  Select Files: <input name="upload[]" type="file" name=".json" multiple>
+  <input type="submit" value="Input Data">
+</form> 
