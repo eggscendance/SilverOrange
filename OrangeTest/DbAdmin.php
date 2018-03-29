e94c3ec4d3e0cecf094e050d4d19489d4e6960bd
Tim <35234836+eggscendance@users.noreply.github.com>
2018-03-29 13:46:32 -0300

    Update DbAdmin.php
    Fleshing out of class, will return to this later in day.
    Combined the DbSelector and DbAdmin (for this small a project don't need separate classes).

--- OrangeTest/DbAdmin.php
+++ OrangeTest/DbAdmin.php
@@ -11,23 +11,69 @@ class DbAdmin
 	private $userName = 'root';
 	private $password = '';
 	private $dbName = 'silver_orange';
+	private $mysqli;
 	
 	/*
 	Default Constructor
 	*/
 	public function __construct()
 	{
-		$dbConnector = new DbConnector($this->serverName, $this->userName, $this->password, $this->dbName);
-		
-		$dbConnector->createConnection();
+		//Upon creating the DbAdmin object, create a connection to the desired database
+		$this->mysqli = mysqli_connect($this->serverName, $this->userName, $this->password, $this->dbName);
+
+		/* check connection */
+		if ($this->mysqli->connect_errno) {
+			printf("Connect failed: %s\n", $this->mysqli->connect_error);
+			exit();
+		}
 	}
 	
 	/*
 	Takes a post ID as its single argument.
-	Retrieves a Post object containing: title, body author
-	Returns data if HTML format
+	Returns a Post object containing: title, body author
 	*/
 	public function getPostById($id)
+	{
+		$id = str_replace(';','',$id); //make sure no semi-colons appear in inserted data
+		
+		//Post members:
+		$postId = '';
+		$postTitle = '';
+		$postBody = '';
+		$postAuthor = '';
+		
+		$sql = "SELECT 
+					posts.id,
+					posts.title,
+					posts.body,
+					authors.full_name
+				FROM posts 
+				LEFT OUTER JOIN authors
+					ON authors.id = posts.author
+				WHERE posts.id = '$id';";
+		
+		if ($result=mysqli_query($this->mysqli,$sql))
+		{
+			// Fetch row
+			while ($row=mysqli_fetch_row($result))
+			{
+				$postId = $row[0];
+				$postTitle = $row[1];
+				$postBody = $row[2];
+				$postAuthor = $row[3];
+			}
+			
+		  // Free result set
+		  mysqli_free_result($result);
+		}
+
+		mysqli_close($this->mysqli); //close our connection
+		
+		$post = new $Post($postId, $postTitle, $postBody, $postAuthor);
+		return $post;
+	}
+	
+	public function insertRecords()
 	{
 		
 	}
