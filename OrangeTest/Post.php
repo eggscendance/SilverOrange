8650a66dd7a98a37ea2f9f4a5c8ceb81e559d8e5
Tim <35234836+eggscendance@users.noreply.github.com>
2018-03-29 12:24:41 -0300

    Update Post.php
    Added basic implementation to Post objects... still under construction.

--- OrangeTest/Post.php
+++ OrangeTest/Post.php
@@ -4,6 +4,43 @@ require_once('DbAdmin.php');
 
 class Post
 {
-
+	private $id = '';
+	private $title = '';
+	private $body = '';
+	private $author = '';
+	
+	/*
+	Default Constructor
+	*/
+	public function __construct($id, $title, $body, $author)
+	{
+		$this->id = $id;
+		$this->title = $title;
+		$this->body = $body;
+		$this->author = $author;
+	}
+	
+	/*
+	Getter Methods
+	*/
+	public getId()
+	{
+		return $id;
+	}
+	
+	public getTitle()
+	{
+		return $title;
+	}
+	
+	public getBody()
+	{
+		return $body;
+	}
+	
+	public getAuthor()
+	{
+		return $author;
+	}
 }
 ?>
