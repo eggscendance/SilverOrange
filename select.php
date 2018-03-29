14e11e4ef678fb9d819699aa6175c856f4d97dab
Tim <35234836+eggscendance@users.noreply.github.com>
2018-03-29 14:56:05 -0300

    Create select.php
    Basic interface for selecting records.

---
+++ select.php
@@ -0,0 +1,6 @@
+<!--BASIC user interface-->
+<p>Select Records:</p>
+<form action="selectProcess.php" method="post" enctype="multipart/form-data">
+  Select ID: <input name="id" type="text">
+  <input type="submit" value="Search">
+</form> 
